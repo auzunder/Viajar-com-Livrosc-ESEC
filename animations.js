5235b601cc7d432169958929f09701f1cfc7fb92
auzunder <edgarnogueiratavares@gmail.com>
2022-01-31 20:46:56 +0000

    efss

---
+++ animations.js
@@ -0,0 +1,15 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById("animaçãoHome"),
+    renderer:"svg",
+    loop: true,
+    autoplay: true,
+    path: "/Imagens/Animações/Animação_I_Livro.json"
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById("backgroundImg"),
+    renderer:"svg",
+    loop: true,
+    autoplay: true,
+    path: "/Imagens/Animações/Animação_I_Background.json"
+})
