0e8445b02c89a539f4542545d2b908b2f57d1766
auzunder <edgarnogueiratavares@gmail.com>
2022-02-06 15:29:24 +0000

    Seta para cima, calendário etc

--- NavbarAnimation.js
+++ NavbarAnimation.js
@@ -1,32 +1,35 @@
-const ANIMATEDCLASSNAME = "animated";
-const ELEMENTS = document.querySelectorAll(".HOVER");
-const ELEMENTS_SPAN = [];
+var ANIMATEDCLASSNAME = "animated";
+var ELEMENTS_SPAN = [];
+var ELEMENTS = document.querySelectorAll(".HOVER");
 
-ELEMENTS.forEach((element, index) => {
-    let addAnimation = false;
-    // Elements that contain the "FLASH" class, add a listener to remove
-    // animation-class when the animation ends
-    if (element.classList[1] == "FLASH") {
-        element.addEventListener("animationend", e => {
-            element.classList.remove(ANIMATEDCLASSNAME);
+window.onload = function () {
+    ELEMENTS = document.querySelectorAll(".HOVER");
+    ELEMENTS.forEach((element, index) => {
+        let addAnimation = false;
+        // Elements that contain the "FLASH" class, add a listener to remove
+        // animation-class when the animation ends
+        if (element.classList[1] == "FLASH") {
+            element.addEventListener("animationend", e => {
+                element.classList.remove(ANIMATEDCLASSNAME);
+            });
+            addAnimation = true;
+        }
+    
+        // If The span element for this element does not exist in the array, add it.
+        if (!ELEMENTS_SPAN[index])
+            ELEMENTS_SPAN[index] = element.querySelector("span");
+    
+        element.addEventListener("mouseover", e => {
+            ELEMENTS_SPAN[index].style.left = e.pageX - element.offsetLeft + "px";
+            ELEMENTS_SPAN[index].style.top = e.pageY - element.offsetTop + "px";
+    
+            // Adicionar class de animação
+            if (addAnimation) element.classList.add(ANIMATEDCLASSNAME);
         });
-        addAnimation = true;
-    }
-
-    // If The span element for this element does not exist in the array, add it.
-    if (!ELEMENTS_SPAN[index])
-        ELEMENTS_SPAN[index] = element.querySelector("span");
-
-    element.addEventListener("mouseover", e => {
-        ELEMENTS_SPAN[index].style.left = e.pageX - element.offsetLeft + "px";
-        ELEMENTS_SPAN[index].style.top = e.pageY - element.offsetTop + "px";
-
-        // Adicionar class de animação
-        if (addAnimation) element.classList.add(ANIMATEDCLASSNAME);
+    
+        element.addEventListener("mouseout", e => {
+            ELEMENTS_SPAN[index].style.left = e.pageX - element.offsetLeft + "px";
+            ELEMENTS_SPAN[index].style.top = e.pageY - element.offsetTop + "px";
+        });
     });
-
-    element.addEventListener("mouseout", e => {
-        ELEMENTS_SPAN[index].style.left = e.pageX - element.offsetLeft + "px";
-        ELEMENTS_SPAN[index].style.top = e.pageY - element.offsetTop + "px";
-    });
-});
+}
--- Structure.js
+++ Structure.js
@@ -1,8 +1,25 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += "responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += "responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
+
+window.onload = function () {
+}
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+$(document).scroll(function () {
+  var y = $(this).scrollTop();
+  if (y > 300) {
+      $('#setaCima').fadeIn();
+  } else {
+      $('#setaCima').fadeOut();
+  }
+
+});
