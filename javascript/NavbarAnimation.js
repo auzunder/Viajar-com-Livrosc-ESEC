0cad5118424b254c5c4260c6e7142ad804a7600c
auzunder <edgarnogueiratavares@gmail.com>
2022-02-10 16:14:28 +0000

    Revert "fds la isto"
    This reverts commit 70e4edc1b2d40c4013d2335c8a16b9c4ec2e4771.

--- javascript/NavbarAnimation.js
+++ javascript/NavbarAnimation.js
@@ -21,13 +21,12 @@ window.onload = function () {
     
         element.addEventListener("mouseover", e => {
             ELEMENTS_SPAN[index].style.left = e.pageX - e.offsetLeft + "px";
-            ELEMENTS_SPAN[index].style.top = element.offsetHeight - e.pageY - element.offsetTop + "px";
+            ELEMENTS_SPAN[index].style.top = 100 - e.pageY - element.offsetTop + "px";
             console.log(ELEMENTS_SPAN[index])
-            console.log("e: ", e)
-            console.log("e.pageX: ", e.pageX, "e.pageY: ", e.pageY)
-            console.log("element: ", element)
-            console.log("element.offsetTop: ", element.offsetTop, "element.offsetLeft: ", element.offsetLeft)
-            console.log(element.offsetHeight - e.pageY - element.offsetTop + "px")
+            console.log(e)
+            console.log(e.pageY)
+            console.log(element.offsetTop)
+            console.log(100 - e.pageY - element.offsetTop + "px")
     
             // Adicionar class de animação
             if (addAnimation) element.classList.add(ANIMATEDCLASSNAME);
