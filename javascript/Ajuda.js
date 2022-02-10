2a58a51b005b510ab9649fed337eb0b0ca119189
auzunder <edgarnogueiratavares@gmail.com>
2022-02-10 22:40:09 +0000

    correções

--- javascript/Ajuda.js
+++ javascript/Ajuda.js
@@ -2,7 +2,7 @@ var opened_answer = 0 //0 quer dizer que nenhuma está aberta
 
 function open_answer(index) {
     for (var i = 0; i < document.getElementsByClassName("questao").length; i++){
-        document.getElementsByClassName("resposta")[i].classList.remove("show");
+        document.getElementsByClassName("resposta")[i].classList.remove("showQA");
         if (i != document.getElementsByClassName("questao").length){
             document.getElementsByClassName("questao")[i].classList.remove("last");
         }
@@ -14,11 +14,11 @@ function open_answer(index) {
         opened_answer = index //altera o 0 para o index (+1) da questão que está aberta
         console.log("element_questao: ", element_questao)
         var element_resposta = document.getElementsByClassName("resposta")[index - 1];
-        element_resposta.classList.add("show");
+        element_resposta.classList.add("showQA");
         console.log("element_resposta: ", element_resposta)
     }
     else {
-        document.getElementsByClassName("resposta")[index].classList.remove("show");
+        document.getElementsByClassName("resposta")[index].classList.remove("showQA");
         if (i != document.getElementsByClassName("questao").length){
             document.getElementsByClassName("questao")[index].classList.remove("last");
         }
