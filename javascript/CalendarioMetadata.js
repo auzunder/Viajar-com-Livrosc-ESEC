5fbdd14f43c796577fff38aadce020ce5d50d711
auzunder <edgarnogueiratavares@gmail.com>
2022-02-15 16:11:56 +0000

    ew

--- javascript/CalendarioMetadata.js
+++ javascript/CalendarioMetadata.js
@@ -9,6 +9,9 @@
 //key:orador -> Nome do Orador
 //key:genero_orador -> Gênero do Orador
 var calendar_books = [
+    {livro: "As Aventuras de sininho", ref:"00001", data: 20220215, hora:18, minuto:20, duração:120, orador: "Carolina Batista", genero_orador: "f", identificaçao_orador: "0001", plataforma:"Zoom"},
+    {livro: "A máquina de retrato", ref:"00006", data: 20220217, hora:07, minuto:30, duração:120, orador: "Duarte Pacheco", genero_orador: "m", identificaçao_orador: "0003", plataforma:"MicrosoftTeams"},
+    {livro: "As Aventuras de sininho", ref:"00001", data: 20220218, hora:11, minuto:20, duração:120, orador: "Rita Moura", genero_orador: "f", identificaçao_orador: "0006", plataforma:"Zoom"},
     {livro: "As Aventuras de sininho", ref:"00001", data: 20220620, hora:11, minuto:20, duração:120, orador: "Carolina Batista", genero_orador: "f", identificaçao_orador: "0001", plataforma:"MicrosoftTeams"},
     {livro: "Todos Devemos ser Feministas", ref:"00002", data: 20220320, hora:14, minuto:00, duração:90, orador: "Josefa Alexandra Pereira Carriço", genero_orador: "f", identificaçao_orador: "0002", plataforma:"Skype"},
     {livro: "Posso Cheirar-te o Rabo?", ref:"00003", data: 20220721, hora:09, minuto:30, duração:70, orador: "Duarte Pacheco", genero_orador: "m", identificaçao_orador: "0003", plataforma:"Zoom"},
@@ -25,6 +28,9 @@ var calendar_books = [
 
 function reporDataOriginal() {
     calendar_books = [
+        {livro: "As Aventuras de sininho", ref:"00001", data: 20220215, hora:18, minuto:20, duração:120, orador: "Carolina Batista", genero_orador: "f", identificaçao_orador: "0001", plataforma:"Zoom"},
+        {livro: "A máquina de retrato", ref:"00006", data: 20220217, hora:07, minuto:30, duração:120, orador: "Duarte Pacheco", genero_orador: "m", identificaçao_orador: "0003", plataforma:"MicrosoftTeams"},
+        {livro: "As Aventuras de sininho", ref:"00001", data: 20220218, hora:11, minuto:20, duração:120, orador: "Rita Moura", genero_orador: "f", identificaçao_orador: "0006", plataforma:"Zoom"},
         {livro: "As Aventuras de sininho", ref:"00001", data: 20220620, hora:11, minuto:20, duração:120, orador: "Carolina Batista", genero_orador: "f", identificaçao_orador: "0001", plataforma:"MicrosoftTeams"},
         {livro: "Todos Devemos ser Feministas", ref:"00002", data: 20220320, hora:14, minuto:00, duração:90, orador: "Josefa Alexandra Pereira Carriço", genero_orador: "f", identificaçao_orador: "0002", plataforma:"Skype"},
         {livro: "Posso Cheirar-te o Rabo?", ref:"00003", data: 20220721, hora:09, minuto:30, duração:70, orador: "Duarte Pacheco", genero_orador: "m", identificaçao_orador: "0003", plataforma:"Zoom"},
@@ -36,7 +42,7 @@ function reporDataOriginal() {
         {livro: "Posso Cheirar-te o Rabo?", ref:"00003", data: 20220421, hora:09, minuto:30, duração:70, orador: "Josefina Ferreira Rodrigues", genero_orador: "f", identificaçao_orador: "0005", plataforma:"Zoom"},
         {livro: "O Leao que temos cá dentro", ref:"00004", data: 20220324, hora:12, minuto:50, duração:120, orador: "Nádia Maia", genero_orador: "f", identificaçao_orador: "0007", plataforma:"Zoom"},
         {livro: "Baleia na banheira", ref:"00005", data: 20220505, hora:16, minuto:00, duração:40, orador: "Jéssica Azevedo", genero_orador: "f", identificaçao_orador: "0008", plataforma:"Skype"},
-        {livro: "A máquina de retrato", ref:"00006", data: 20220206, hora:07, minuto:30, duração:120, orador: "José Pereira Fernades Daniel", genero_orador: "m", identificaçao_orador: "0009", plataforma:"Presencial"}
+    {livro: "A máquina de retrato", ref:"00006", data: 20220206, hora:07, minuto:30, duração:120, orador: "José Pereira Fernades Daniel", genero_orador: "m", identificaçao_orador: "0009", plataforma:"Presencial"}
     ]
     window.location.hash = ''
     defaultFilters()
