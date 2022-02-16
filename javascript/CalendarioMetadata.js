6e6706019fc30eab01dccd740dbf526fc9def13b
auzunder <edgarnogueiratavares@gmail.com>
2022-02-16 00:07:58 +0000

    dwada

--- javascript/CalendarioMetadata.js
+++ javascript/CalendarioMetadata.js
@@ -13,17 +13,17 @@ var calendar_books = [
     {livro: "A máquina de retrato", ref:"00006", data: 20220217, hora:07, minuto:30, duração:120, orador: "Duarte Pacheco", genero_orador: "m", identificaçao_orador: "0003", plataforma:"MicrosoftTeams"},
     {livro: "As Aventuras de sininho", ref:"00001", data: 20220218, hora:11, minuto:20, duração:120, orador: "Rita Moura", genero_orador: "f", identificaçao_orador: "0006", plataforma:"Zoom"},
     {livro: "As Aventuras de sininho", ref:"00001", data: 20220620, hora:11, minuto:20, duração:120, orador: "Carolina Batista", genero_orador: "f", identificaçao_orador: "0001", plataforma:"MicrosoftTeams"},
-    {livro: "Todos Devemos ser Feministas", ref:"00002", data: 20220320, hora:14, minuto:00, duração:90, orador: "Josefa Alexandra Pereira Carriço", genero_orador: "f", identificaçao_orador: "0002", plataforma:"Skype"},
+    {livro: "Todos Devemos ser Feministas", ref:"00002", data: 20220320, hora:14, minuto:00, duração:90, orador: "Josefa Carriço", genero_orador: "f", identificaçao_orador: "0002", plataforma:"Skype"},
     {livro: "Posso Cheirar-te o Rabo?", ref:"00003", data: 20220721, hora:09, minuto:30, duração:70, orador: "Duarte Pacheco", genero_orador: "m", identificaçao_orador: "0003", plataforma:"Zoom"},
     {livro: "O Leao que temos cá dentro", ref:"00004", data: 20220324, hora:12, minuto:50, duração:120, orador: "Yasmin Assunção", genero_orador: "f", identificaçao_orador: "0004", plataforma:"Zoom"},
-    {livro: "Baleia na banheira", ref:"00005", data: 20220205, hora:16, minuto:00, duração:40, orador: "Josefina Ferreira Rodrigues", genero_orador: "f", identificaçao_orador: "0005", plataforma:"Zoom"},
+    {livro: "Baleia na banheira", ref:"00005", data: 20220205, hora:16, minuto:00, duração:40, orador: "Josefina Rodrigues", genero_orador: "f", identificaçao_orador: "0005", plataforma:"Zoom"},
     {livro: "A máquina de retrato", ref:"00006", data: 20220206, hora:07, minuto:30, duração:120, orador: "Duarte Pacheco", genero_orador: "m", identificaçao_orador: "0003", plataforma:"MicrosoftTeams"},
     {livro: "As Aventuras de sininho", ref:"00001", data: 20220120, hora:11, minuto:20, duração:120, orador: "Rita Moura", genero_orador: "f", identificaçao_orador: "0006", plataforma:"Zoom"},
-    {livro: "Todos Devemos ser Feministas", ref:"00002", data: 20220420, hora:14, minuto:00, duração:90, orador: "Josefa Alexandra Pereira Carriço", genero_orador: "f", identificaçao_orador: "0002", plataforma:"Zoom"},
-    {livro: "Posso Cheirar-te o Rabo?", ref:"00003", data: 20220421, hora:09, minuto:30, duração:70, orador: "Josefina Ferreira Rodrigues", genero_orador: "f", identificaçao_orador: "0005", plataforma:"Zoom"},
+    {livro: "Todos Devemos ser Feministas", ref:"00002", data: 20220420, hora:14, minuto:00, duração:90, orador: "Josefa Carriço", genero_orador: "f", identificaçao_orador: "0002", plataforma:"Zoom"},
+    {livro: "Posso Cheirar-te o Rabo?", ref:"00003", data: 20220421, hora:09, minuto:30, duração:70, orador: "Josefina Rodrigues", genero_orador: "f", identificaçao_orador: "0005", plataforma:"Zoom"},
     {livro: "O Leao que temos cá dentro", ref:"00004", data: 20220324, hora:12, minuto:50, duração:120, orador: "Nádia Maia", genero_orador: "f", identificaçao_orador: "0007", plataforma:"Zoom"},
     {livro: "Baleia na banheira", ref:"00005", data: 20220505, hora:16, minuto:00, duração:40, orador: "Jéssica Azevedo", genero_orador: "f", identificaçao_orador: "0008", plataforma:"Skype"},
-    {livro: "A máquina de retrato", ref:"00006", data: 20220206, hora:07, minuto:30, duração:120, orador: "José Pereira Fernades Daniel", genero_orador: "m", identificaçao_orador: "0009", plataforma:"Presencial"}
+    {livro: "A máquina de retrato", ref:"00006", data: 20220206, hora:07, minuto:30, duração:120, orador: "José Daniel", genero_orador: "m", identificaçao_orador: "0009", plataforma:"Presencial"}
 ]
 
 function reporDataOriginal() {
@@ -32,17 +32,17 @@ function reporDataOriginal() {
         {livro: "A máquina de retrato", ref:"00006", data: 20220217, hora:07, minuto:30, duração:120, orador: "Duarte Pacheco", genero_orador: "m", identificaçao_orador: "0003", plataforma:"MicrosoftTeams"},
         {livro: "As Aventuras de sininho", ref:"00001", data: 20220218, hora:11, minuto:20, duração:120, orador: "Rita Moura", genero_orador: "f", identificaçao_orador: "0006", plataforma:"Zoom"},
         {livro: "As Aventuras de sininho", ref:"00001", data: 20220620, hora:11, minuto:20, duração:120, orador: "Carolina Batista", genero_orador: "f", identificaçao_orador: "0001", plataforma:"MicrosoftTeams"},
-        {livro: "Todos Devemos ser Feministas", ref:"00002", data: 20220320, hora:14, minuto:00, duração:90, orador: "Josefa Alexandra Pereira Carriço", genero_orador: "f", identificaçao_orador: "0002", plataforma:"Skype"},
+        {livro: "Todos Devemos ser Feministas", ref:"00002", data: 20220320, hora:14, minuto:00, duração:90, orador: "Josefa Carriço", genero_orador: "f", identificaçao_orador: "0002", plataforma:"Skype"},
         {livro: "Posso Cheirar-te o Rabo?", ref:"00003", data: 20220721, hora:09, minuto:30, duração:70, orador: "Duarte Pacheco", genero_orador: "m", identificaçao_orador: "0003", plataforma:"Zoom"},
         {livro: "O Leao que temos cá dentro", ref:"00004", data: 20220324, hora:12, minuto:50, duração:120, orador: "Yasmin Assunção", genero_orador: "f", identificaçao_orador: "0004", plataforma:"Zoom"},
-        {livro: "Baleia na banheira", ref:"00005", data: 20220205, hora:16, minuto:00, duração:40, orador: "Josefina Ferreira Rodrigues", genero_orador: "f", identificaçao_orador: "0005", plataforma:"Zoom"},
+        {livro: "Baleia na banheira", ref:"00005", data: 20220205, hora:16, minuto:00, duração:40, orador: "Josefina Rodrigues", genero_orador: "f", identificaçao_orador: "0005", plataforma:"Zoom"},
         {livro: "A máquina de retrato", ref:"00006", data: 20220206, hora:07, minuto:30, duração:120, orador: "Duarte Pacheco", genero_orador: "m", identificaçao_orador: "0003", plataforma:"MicrosoftTeams"},
         {livro: "As Aventuras de sininho", ref:"00001", data: 20220120, hora:11, minuto:20, duração:120, orador: "Rita Moura", genero_orador: "f", identificaçao_orador: "0006", plataforma:"Zoom"},
-        {livro: "Todos Devemos ser Feministas", ref:"00002", data: 20220420, hora:14, minuto:00, duração:90, orador: "Josefa Alexandra Pereira Carriço", genero_orador: "f", identificaçao_orador: "0002", plataforma:"Zoom"},
-        {livro: "Posso Cheirar-te o Rabo?", ref:"00003", data: 20220421, hora:09, minuto:30, duração:70, orador: "Josefina Ferreira Rodrigues", genero_orador: "f", identificaçao_orador: "0005", plataforma:"Zoom"},
+        {livro: "Todos Devemos ser Feministas", ref:"00002", data: 20220420, hora:14, minuto:00, duração:90, orador: "Josefa Carriço", genero_orador: "f", identificaçao_orador: "0002", plataforma:"Zoom"},
+        {livro: "Posso Cheirar-te o Rabo?", ref:"00003", data: 20220421, hora:09, minuto:30, duração:70, orador: "Josefina Rodrigues", genero_orador: "f", identificaçao_orador: "0005", plataforma:"Zoom"},
         {livro: "O Leao que temos cá dentro", ref:"00004", data: 20220324, hora:12, minuto:50, duração:120, orador: "Nádia Maia", genero_orador: "f", identificaçao_orador: "0007", plataforma:"Zoom"},
         {livro: "Baleia na banheira", ref:"00005", data: 20220505, hora:16, minuto:00, duração:40, orador: "Jéssica Azevedo", genero_orador: "f", identificaçao_orador: "0008", plataforma:"Skype"},
-    {livro: "A máquina de retrato", ref:"00006", data: 20220206, hora:07, minuto:30, duração:120, orador: "José Pereira Fernades Daniel", genero_orador: "m", identificaçao_orador: "0009", plataforma:"Presencial"}
+    {livro: "A máquina de retrato", ref:"00006", data: 20220206, hora:07, minuto:30, duração:120, orador: "José Daniel", genero_orador: "m", identificaçao_orador: "0009", plataforma:"Presencial"}
     ]
     window.location.hash = ''
     defaultFilters()
@@ -342,6 +342,26 @@ function printSessions(clear) {
             +'</p> </div> <button class="boxInnerOutterShadow inscrição" id="inscrição_ref_'+ livro.ref 
             +'" type="button" onclick="abrir()">Inscrever na sessão</button> </div> <div class="imgLivro"> <img class="imgSrcCapaLivro" src="/Imagens/Calendário/Livro_'+ livro.ref 
             +'.jpg"> </div> </div> </div>'
+        }
+    }
+}
+
+function tableSessions(sessoes){
+    //Identificação do corpo da tabela
+    var table = document.getElementById("tableBody")
+
+    //Limpar tabela
+    table.innerHTML = "";
+
+    //Controlo de numero de sessoes a apresentar
+    var count = 0
+
+    //Adicionar as sessoes à tabela
+    for (livro of calendar_books){
+        count += 1;
+        table.innerHTML += '<tr><td class="sessionInfoElement"><a href="Calendario.html">'+ livro.orador +'</a></td><td class="sessionInfoElement"><a href="Calendario.html">'+ livro.livro +'</a></td><td class="sessionInfoElement"><a href="Calendario.html">'+ reverse(livro.data) +'</a></td></tr></a>';
+        if (count == sessoes){
+            break
         }
     }
 }
